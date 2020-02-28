6e8ac2f8587a2069272f337d8ed1ab0648ddb814
logan <lbbogert@iastate.edu>
2020-02-28 10:40:40 -0600

    Ingrained Fear working

--- src/main/java/intoDarkness/DefaultMod.java
+++ src/main/java/intoDarkness/DefaultMod.java
@@ -34,6 +34,7 @@ import intoDarkness.relics.DefaultClickableRelic;
 import intoDarkness.relics.PlaceholderRelic;
 import intoDarkness.relics.PlaceholderRelic2;
 import intoDarkness.relics.TestRelic;
+import intoDarkness.relics.IngrainedFear;
 
 import intoDarkness.util.IDCheckDontTouchPls;
 import intoDarkness.util.TextureLoader;
@@ -383,6 +384,7 @@ public class DefaultMod implements
         // This adds a relic to the Shared pool. Every character can find this relic.
         BaseMod.addRelic(new PlaceholderRelic2(), RelicType.SHARED);
         BaseMod.addRelic(new TestRelic(), RelicType.SHARED);
+        BaseMod.addRelic(new IngrainedFear(), RelicType.SHARED);
         
         // Mark relics as seen (the others are all starters so they're marked as seen in the character file
         UnlockTracker.markRelicAsSeen(BottledPlaceholderRelic.ID);
